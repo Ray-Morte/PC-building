239fbbc1a08b69cc7663c0cb2519a88a4c2be3bd
Ray-Morte <ray.morte.harrison@gmail.com>
2025-11-08 16:35:40 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 const contents = document.querySelectorAll('.program-line__content')
+const closedescr = document.querySelectorAll(".program-line__descr")
+
 
 contents.forEach((elem) => {
     const title = elem.querySelector(".program-line__title")
     const descr = elem.querySelector(".program-line__descr")
-
+    
+       closedescr.forEach((element) => {
     title.addEventListener("click", () =>  {
-        descr.classList.toggle("active")
+        element.classList.remove("active")
     })
 
+/*     title.addEventListener("click", () => {
+        closing.classList.remove("active")
+    }) */
+    
+    title.addEventListener("click", () =>  {
+        descr.classList.add("active")
+    })
+    
+
+})
 /*     console.dir(descr.classList.remove) */
 })
